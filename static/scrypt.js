05ea2ad8f1bfda3c570c8b0004cbe48227131b74
unknown <bahp2003@gmail.com>
2024-01-05 10:11:12 +0300

    frenf

--- static/scrypt.js
+++ static/scrypt.js
@@ -1,62 +1,188 @@
+
 const back = document.getElementById("back");
 const next = document.getElementById("next");
 const search = document.getElementById("search");
 const fill = document.getElementById("fill");
-let table = document.getElementById("table");
+let table = document.getElementById('tableRoute')
+let items = document.querySelectorAll('.pagination li')
+let iPages = 10;            //кол-во марш на стр
+
+let htmlTable = `                    <tbody>
+
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>
+        <button class="select-button" onclick="">Выбрать</button>
+        <div class="checkbox-list">
+            <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
+            <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
+            <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
+        </div>
+    </td>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>
+        <button class="select-button" onclick="">Выбрать</button>
+        <div class="checkbox-list">
+            <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
+            <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
+            <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
+        </div>
+    </td>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>
+        <button class="select-button" onclick="">Выбрать</button>
+        <div class="checkbox-list">
+            <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
+            <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
+            <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
+        </div>
+    </td>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>
+        <button class="select-button" onclick="">Выбрать</button>
+        <div class="checkbox-list">
+            <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
+            <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
+            <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
+        </div>
+    </td>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>
+        <button class="select-button" onclick="">Выбрать</button>
+        <div class="checkbox-list">
+            <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
+            <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
+            <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
+        </div>
+    </td>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>
+        <button class="select-button" onclick="">Выбрать</button>
+        <div class="checkbox-list">
+            <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
+            <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
+            <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
+        </div>
+    </td>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>
+        <button class="select-button" onclick="">Выбрать</button>
+        <div class="checkbox-list">
+            <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
+            <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
+            <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
+        </div>
+    </td>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>
+        <button class="select-button" onclick="">Выбрать</button>
+        <div class="checkbox-list">
+            <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
+            <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
+            <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
+        </div>
+    </td>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>
+        <button class="select-button" onclick="">Выбрать</button>
+        <div class="checkbox-list">
+            <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
+            <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
+            <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
+        </div>
+    </td>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>
+        <button class="select-button" onclick="">Выбрать</button>
+        <div class="checkbox-list">
+            <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
+            <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
+            <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
+        </div>
+    </td>
+</tr>
+
+</tbody>`
 
 async function getData(){
-    const dannie = await fetch("");
-    const data = await dannie.json();
+    const responce = await fetch('http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=e228b9a7-e4f8-4fc7-8b70-0974917b46fd');
+    const data = await responce.json();
     return data;
 }
 
 
-next.onclick = function(){
-    table=table.insertAdjacentHTML("beforeend", `<table id="tableRoute">
-    <thead>
-        <tr>
-            <th>Название1</th>
-            <th>Описание1</th>
-            <th>Объекты1</th>
-            <th>Достопримечательности1</th>
-        </tr>
-    </thead>
 
-    <tbody>
-        <tr>
-            <td>Новичек1</td>
-            <td>Для новичков1</td>
-            <td>Красная площадь1</td>
-            <td>
-                <button class="select-button" onclick="">Выбрать1</button>
-                <div class="checkbox-list">
-                    <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
-                    <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
-                    <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
-                </div>
-            </td>
-        </tr>
+let tBody = document.createElement('tbody');
 
-        <tr>
-            <td>почти новичек1</td>
-            <td>Для почти новичков1</td>
-            <td>Красная площадь и москва сити1</td>
-            <td>
-                <button class="select-button" onclick="">Выбрать1</button>
-                <div class="checkbox-list">
-                    <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
-                    <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
-                    <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
-                </div>
-            </td>
-        </tr>
-    </tbody>
-</table>` )
+for (let item of items)
+{
+    item.addEventListener('click', async function(){
+        let thisPage = Number(this.innerHTML);
+        let start = (thisPage-1)*iPages;
+        let end = start + iPages;
+        let zapisi = (await getData()).slice(start, end);
 
+        let htmlTR = "";
+        for(let zapis of zapisi)
+        {
+            htmlTR += `<tr>
+                <td>${zapis.name}</td> 
+                <td>${zapis.description}</td> 
+                <td>${zapis.mainObject}</td> 
+                <td>
+                    <button class="select-button" onclick="">Выбрать</button>
+                    <div class="checkbox-list">
+                        <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
+                        <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
+                        <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
+                    </div>
+                </td>
+            </tr>`;
+            tBody.innerHTML = htmlTR;
+            table.appendChild(tBody);
+        }
+    });
 }
 
-search.oninput = function(){
-    console.log("Pidr" + search.value);
-}
 
-console.log(search.id);
+
+
+
