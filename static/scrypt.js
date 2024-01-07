8d2ca4899919173f97e6e26e3e3432bfa4f173d4
unknown <bahp2003@gmail.com>
2024-01-07 05:53:30 +0300

    pagF

--- static/scrypt.js
+++ static/scrypt.js
@@ -14,7 +14,7 @@ let thisPage = 0;
 async function getData() {
     const responce = await fetch('http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=e228b9a7-e4f8-4fc7-8b70-0974917b46fd');
     const data = await responce.json();
-    console.log(typeof(data.length));
+    //console.log(typeof(data.length));
     return data;
 }
 
@@ -41,7 +41,7 @@ async function pagDraw() {
 
 
     for (let item of items) {
-        item.addEventListener('click', async function () {
+        item.addEventListener('click', async function () {                    //DOM = item
             thisPage = Number(this.innerHTML);
             let start = (thisPage - 1) * iPages;
             let end = start + iPages;
@@ -56,11 +56,6 @@ async function pagDraw() {
                 <td>${zapis.mainObject}</td> 
                 <td>
                     <button class="select-button" onclick="">Выбрать</button>
-                    <div class="checkbox-list">
-                        <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
-                        <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
-                        <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
-                    </div>
                 </td>
             </tr>`;
                 tBody.innerHTML = htmlTR;
@@ -84,11 +79,6 @@ async function pagDraw() {
                 <td>${zapis.mainObject}</td> 
                 <td>
                     <button class="select-button" onclick="">Выбрать</button>
-                    <div class="checkbox-list">
-                        <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
-                        <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
-                        <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
-                    </div>
                 </td>
             </tr>`;
             tBody.innerHTML = htmlTR;
@@ -111,39 +101,59 @@ async function pagDraw() {
                 <td>${zapis.mainObject}</td> 
                 <td>
                     <button class="select-button" onclick="">Выбрать</button>
-                    <div class="checkbox-list">
-                        <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
-                        <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
-                        <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
-                    </div>
                 </td>
             </tr>`;
             tBody.innerHTML = htmlTR;
             table.appendChild(tBody);
         }
     })
+
+    // Добавление слушателей событий для кнопок "Выбрать"
+    addSelectButtonListeners();
+
+
 }
 
+async function nachP() {
+    thisPage = 1;
+    let start = (thisPage - 1) * iPages;
+    let end = start + iPages;
 
-pagDraw();
+    let zapisi = (await getData()).slice(start, end);
 
+    let htmlTR = "";
+    for (let zapis of zapisi) {
+        htmlTR += `<tr>
+                <td>${zapis.name}</td> 
+                <td>${zapis.description}</td> 
+                <td>${zapis.mainObject}</td> 
+                <td>
+                    <button class="select-button" onclick="">Выбрать</button>
+                </td>
+            </tr>`;
+        tBody.innerHTML = htmlTR;
+        table.appendChild(tBody);
+    }
+}
 
+nachP();
+
+pagDraw()
 
 function searchPodstr(str, pstr) {
     let k = 0;
-    let rez=[];
+    let rez = [];
     for (let q = 0; q < str.length; q++) {
         k = str[q].name.indexOf(pstr);
         if (k !== -1) {
 
             for (let i = 0; i < pstr.length; i++) {
 
-                if (pstr[i] != str[q].name[k+i]) {
+                if (pstr[i] != str[q].name[k + i]) {
                     break;
                 }
 
-                if((pstr[i] == str[q].name[k+i]) && (i==pstr.length-1))
-                {
+                if ((pstr[i] == str[q].name[k + i]) && (i == pstr.length - 1)) {
                     rez.push(str[q].id);
                 }
             }
@@ -153,42 +163,32 @@ function searchPodstr(str, pstr) {
 }
 
 sB.onclick = function () {
-    getData().then((datJ)=>{
-        let DATA= []
-        let zapisi= []
+    thisPage = 1
+    getData().then((datJ) => {
+        let DATA = []
+        let zapisi = []
         let newData = searchPodstr(datJ, search.value);
-        
-        for(let i=0; i<newData.length; i++)
-        {
-            DATA.push(datJ[newData[i]-2])
+
+        for (let i = 0; i < newData.length; i++) {
+            DATA.push(datJ[newData[i] - 2])
         }
 
-        thisPage = Number(this.innerHTML);
         let start = (thisPage - 1) * iPages;
         let end = start + iPages;
-        console.log("start", start)
-        console.log("thisPage", thisPage)
-        console.log("end", end)
 
-        DATA.length>10? zapisi = DATA.slice(start, end): 
-        zapisi = DATA
-        console.log("zapisi", zapisi)
+        zapisi = DATA.slice(start, end)
+        thisPage += end;
 
         let htmlTR = "";
         for (let zapis of zapisi) {
             htmlTR += `<tr>
-                <td>${zapis.name}</td> 
-                <td>${zapis.description}</td> 
-                <td>${zapis.mainObject}</td> 
-                <td>
-                    <button class="select-button" onclick="">Выбрать</button>
-                    <div class="checkbox-list">
-                        <input type="checkbox" id="checkbox1"> <label for="checkbox1">1</label><br>
-                        <input type="checkbox" id="checkbox2"> <label for="checkbox2">2</label><br>
-                        <input type="checkbox" id="checkbox3"> <label for="checkbox3">3</label>
-                    </div>
-                </td>
-            </tr>`;
+                    <td>${zapis.name}</td> 
+                    <td>${zapis.description}</td> 
+                    <td>${zapis.mainObject}</td> 
+                    <td>
+                        <button class="select-button" onclick="">Выбрать</button>
+                    </td>
+                </tr>`;
             tBody.innerHTML = htmlTR;
             table.appendChild(tBody);
         }
@@ -196,4 +196,42 @@ sB.onclick = function () {
     })
 }
 
+async function selB(button) {
+    // Получаем ближайший родительский элемент tr
+    let row = button;
+    while (row && row.tagName !== 'TR') {
+        row = row.parentNode;
+    }
 
+    // Проверяем, был ли найден родительский элемент
+    if (row && row.tagName === 'TR') {
+        // Изменяем или добавляем стиль фона строки
+        if (row.classList.contains('selected-row')) {
+            row.style.backgroundColor = ''; // Убрать цвет фона
+            row.classList.remove('selected-row');
+        } else {
+            row.style.backgroundColor = 'red'; // Установить цвет фона в красный
+            row.classList.add('selected-row');
+        }
+    } else {
+        console.log("Родительский элемент <tr> не найден.");
+    }
+}
+
+function addSelectButtonListeners() {
+    document.querySelectorAll('.select-button').forEach(button => {
+        console.log("Click 2")
+        button.addEventListener('click', handleSelectButtonClick);
+    });
+}
+
+function handleSelectButtonClick(event) {                                      //нажатие кнопки "Выбрать"
+    const selectedRow = event.target.closest('tr');
+    const currentColor = selectedRow.style.backgroundColor;
+
+    if (currentColor === 'rgb(143, 197, 248)') {
+        selectedRow.style.backgroundColor = ''; 
+    } else {
+        selectedRow.style.backgroundColor = 'rgb(143, 197, 248)';
+    }
+}
